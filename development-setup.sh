dc63b706483d3375f24e9404bb80c1bf2faca5d6
Carlos Henrique Oliveira Coêlho <chocoelho@localhost.localdomain>
2018-01-15 00:26:35 -0300

    Fix linting issues on development-setup.sh

--- development-setup.sh
+++ development-setup.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-if [ -f /etc/lsb-release ]; then
-  . /etc/lsb-release
+# a lot of distro's which have /etc/os-release will not have lsb_release
+# installed as default (such as Arch Linux) 
+if [ -f /etc/lsb-release ] || [ -f /etc/os-release ]; then
+  # reference: https://github.com/brocaar/lora-gateway-bridge/issues/30#issue-213360185
+  DISTRIB_ID=$(lsb_release -si)
+  DISTRIB_CODENAME=$(lsb_release -sc)
+  export DISTRIB_ID
+  export DISTRIB_CODENAME
   OS=$DISTRIB_ID
 elif [ -f /etc/debian_version ]; then
   OS=Debian
@@ -15,8 +21,8 @@ else
   OS=$(uname -s)
 fi
 
-if [ -x ./development-setup/$OS.sh ]; then
-  ./development-setup/$OS.sh
+if [ -x "./development-setup/$OS.sh" ]; then
+  "./development-setup/$OS.sh"
   ./holodev setup
 else
   echo "W: ./development-setup/$OS.sh not found, aborting setup for holodev!"
